37fb5dfd75de6133c87a70dc85b70beee85f2c2e
Asher-P <hhhh52111@gmail.com>
2021-03-18 13:32:30 +0200

    with subcimponent filter

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -7,8 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilterComponent implements OnInit {
 
+  distance:number;
+  publisher:string;
+  dataDateFilter={
+  fromDate:Date,
+  toDate:Date}
+  tags:string[];
+  hashtags:string[];
   constructor() { }
 
+  submitPost(event){
+    event.preventDefault();
+    
+  }
+  ClearFilters(){
+    this.distance=null;
+    this.publisher=null;
+    this.dataDateFilter.fromDate=null;
+    this.dataDateFilter.toDate=null;
+    this.tags=null;
+    this.hashtags=null;
+  }
+  DateSelected(event){
+    this.dataDateFilter = event;
+  }
+  onDistanceSelected(event){
+    this.distance = event;
+  }
+  onHashtagsSelected(event){
+    this.hashtags=event;
+    console.log(this.hashtags)
+  }
+  onPublisherSelected(event){
+    this.publisher = event;
+  }
+  onTagsSelected(event){
+    this.tags = event;
+  }
   ngOnInit(): void {
   }
 
