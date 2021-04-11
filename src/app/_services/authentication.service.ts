7bd95c9a7a65f8345342355bed167b91da076115
#77104232 <reem.cohen39@gmail.com>
2021-04-12 01:03:11 +0300

    fixed - send token in headers

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,5 +1,5 @@
 ﻿import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders,HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import * as env from '../../environments/authenticationEnv'
@@ -18,46 +18,47 @@ export class AuthenticationService {
     public currentUser: Observable<User>;
     private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-    constructor(private http: HttpClient,private cookieService:CookieService,private userService:UserService) {
+    constructor(private http: HttpClient, private cookieService: CookieService, private userService: UserService) {
         this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
         this.currentUser = this.currentUserSubject.asObservable();
     }
 
     public get currentUserValue(): User {
         return this.currentUserSubject.value;
-    }   
-    async isAuthenticated(){
-        let params = new HttpParams();
+    }
+    async isAuthenticated() {
+        let headers = new HttpHeaders();
         let facebookData;
-     facebookData = await this.http.get(`${AuthenticationEnv.facebookVerifyApi}${this.cookieService.get("accessToken")}`)
-     .toPromise()
-     .catch(error=>console.warn("error is " + error));
-     console.log("facebookData",facebookData);
-     if(!this.userService.currentUser)
-        this.userService.setUser(facebookData);
-        params = params.append('token', this.cookieService.get("token"));
-       let localAuth :boolean|Object=false;
-       localAuth = await this.http.get("http://localhost:6060/islogin",{params:params})
-       .toPromise().catch(err=>localAuth=false);
-       //(res=>console.log("res",<boolean>res));
-       console.log("localAuth",localAuth);
-       if(localAuth || facebookData){
+        facebookData = await this.http.get(`${AuthenticationEnv.facebookVerifyApi}${this.cookieService.get("accessToken")}`)
+            .toPromise()
+            .catch(error => console.warn("error is " + error));
+        console.log("facebookData", facebookData);
+        if (!this.userService.currentUser)
+            this.userService.setUser(facebookData);
+        headers = headers.append('token', "Bearer " + this.cookieService.get('token'));
+        let localAuth: boolean | Object = false;
+        localAuth = await this.http.get("http://localhost:6060/islogin", { headers })
+            .toPromise().catch(err => localAuth = false);
+        //(res=>console.log("res",<boolean>res));
+        console.log("localAuth", localAuth);
+        if (localAuth || facebookData) {
             return true;
         }
-       return false;
-      }
+        return false;
+    }
 
-    login(user:User):Observable<string>{
+    login(user: User): Observable<any> {
         console.log(JSON.stringify(user));
-       return this.http.post(`${AuthenticationEnv.apiUrl}/login`,JSON.stringify(user),{headers:this.headers})
-        .pipe(map((res:any) =>{
-          if(res.token){
-          return(res.token)}
-        }));
-      }
+        return this.http.post(`${AuthenticationEnv.apiUrl}/login`, JSON.stringify(user), { headers: this.headers })
+            .pipe(map((res: any) => {
+                if (res.token) {
+                    return (res)
+                }
+            }));
+    }
 
 
-    register(user:User) {
+    register(user: User) {
         return this.http.post<any>(this.registerUrl, user)
     }
 
